05690ceaa3b44185c44fa14d9a0b51bce77d2cea
hackerAlice <sudo_rm@163.com>
2021-04-25 16:44:28 +0800

    完成iatoltsTree的转换

--- parserlib/iatotree.py
+++ parserlib/iatotree.py
@@ -2,11 +2,14 @@
 
 from lib.node import Node
 from ia.interfaceautomat import InterfaceAutomata
+from ia.state import State as IaState
 from lib.lts_tree import LTSTree
 from lib.state import State as LtsState
+from parserlib.common import get_node_children
+import queue
 
 
-def ia_parser_to_tree(ia: InterfaceAutomata):
+def ia_parser_to_tree(ia: InterfaceAutomata) -> LTSTree:
     """
     该函数通过InterfaceAutomata的一个实例装换为LtsTree实例
     :param ia: InterfaceAutomata实例对象
@@ -15,7 +18,17 @@ def ia_parser_to_tree(ia: InterfaceAutomata):
     init_state = ia.state_init
     root_state = LtsState(init_state.state_name)
     root = Node(None, root_state)
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        node = q.get()
+        ia_state = IaState(node.state.state_name)
+        node.children = get_node_children(ia, ia_state)
+        if node.children:
+            for child in node.children:
+                q.put(child)
 
     lts_tree = LTSTree(root)
 
-    #
+    return lts_tree
