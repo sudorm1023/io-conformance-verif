1911146fbd55ade63ce7669093c5a963cf640632
hackerAlice <sudo_rm@163.com>
2021-05-31 09:35:29 +0800

    增加新的判别方法的测试用例

---
+++ lib/test/test_io_conformance2.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+import unittest
+from lib.lts_tree import LTSTree
+from lib.node import Node
+from lib.state import State
+from lib.action import Action
+from lib.action import ActionEnum
+
+
+class TestIoConformation(unittest.TestCase):
+
+    def test_testcase1(self):
+        """
+        测试用例如下：
+        implementation：s0 -act1?-> s1
+        specification: s0 -act1?-> s1
+        :return: None
+        """
+        imp_root = Node(None, State("s0"))
+        imp_root.children = [Node(Action("act1", ActionEnum.INPUT), State("s1"))]
+        spec_root = Node(None, State("s0"))
+        spec_root.children = [Node(Action("act1", ActionEnum.INPUT), State("s1"))]
+        imp = LTSTree(imp_root)
+        imp.trace()
+        spec = LTSTree(spec_root)
+        spec.trace()
+        self.assertTrue(imp.io_conform2(spec), msg="io not conform")
+
+    def test_testcase2(self):
+        """
+        测试用例如下：
+        implementation: s0 -but?-> s1, s1 -liq!-> s2
+        specification: s0 -but?-> s1, s1 -liq!-> s2, s1 -choc-> s3
+        :return: None
+        """
+        # implementation
+        imp_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("liq", ActionEnum.OUTPUT), State("s2"))
+        imp_root.children = [node1]
+        node1.children = [node2]
+        imp = LTSTree(imp_root)
+        imp.trace()
+
+        # specification
+        spec_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("liq", ActionEnum.OUTPUT), State("s2"))
+        node3 = Node(Action("choc", ActionEnum.OUTPUT), State("s3"))
+        spec_root.children = [node1]
+        node1.children = [node2, node3]
+        spec = LTSTree(spec_root)
+        spec.trace()
+
+        self.assertTrue(imp.io_conform2(spec), msg="io not conform")
+
+    def test_testcase3(self):
+        """
+        测试用例如下：
+        implementation: s0 -but?-> s1, s1 -liq!-> s2, s1 -choc!-> s3
+        specification: s0 -but?-> s1, s1 -liq!-> s2
+        :return:
+        """
+        # implementation
+        imp_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("liq", ActionEnum.OUTPUT), State("s2"))
+        node3 = Node(Action("choc", ActionEnum.OUTPUT), State("s3"))
+        imp_root.children = [node1]
+        node1.children = [node2, node3]
+        imp = LTSTree(imp_root)
+        imp.trace()
+
+        # specification
+        spec_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("liq", ActionEnum.OUTPUT), State("s2"))
+        spec_root.children = [node1]
+        node1.children = [node2]
+        spec = LTSTree(spec_root)
+        spec.trace()
+
+        self.assertFalse(imp.io_conform2(spec), msg="Error, test result: io conform!")
+
+    def test_testcase4(self):
+        """
+        测试用例如下：
+        implementation: s0 -but?-> s1, s1 -liq!-> s2
+        specification: s0 -but?-> s1, s0 -but?-> s2, s1 -liq!-> s3, s2 -but?-> s4, s4 -choc!-> s5
+        :return: None
+        """
+        # implementation
+        imp_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("liq", ActionEnum.OUTPUT), State("s2"))
+        imp_root.children = [node1]
+        node1.children = [node2]
+        imp = LTSTree(imp_root)
+        imp.trace()
+
+        # specification
+        spec_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("but", ActionEnum.INPUT), State("s2"))
+        spec_root.children = [node1, node2]
+        node3 = Node(Action("liq", ActionEnum.OUTPUT), State("s3"))
+        node1.children = [node3]
+        node4 = Node(Action("but", ActionEnum.INPUT), State("s4"))
+        node2.children = [node4]
+        node5 = Node(Action("liq", ActionEnum.OUTPUT), State("s5"))
+        node4.children = [node5]
+        spec = LTSTree(spec_root)
+        spec.trace()
+
+        self.assertFalse(imp.io_conform2(spec), msg="Error, test result: io conform!")
+
+    def test_testcase5(self):
+        """
+        测试用例如下：
+        implementation: s0 -but?-> s1, s1 -liq!-> s2, s1 -choc!-> s3
+        specification: s0 -but?-> s1, s0 -but?-> s2, s1 -liq!-> s3, s2 -but?-> s4, s4 -choc!-> s5
+        :return: None
+        """
+        # implementation
+        imp_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("liq", ActionEnum.OUTPUT), State("s2"))
+        node3 = Node(Action("choc", ActionEnum.OUTPUT), State("s3"))
+        imp_root.children = [node1]
+        node1.children = [node2, node3]
+        imp = LTSTree(imp_root)
+        imp.trace()
+
+        # specification
+        spec_root = Node(None, State("s0"))
+        node1 = Node(Action("but", ActionEnum.INPUT), State("s1"))
+        node2 = Node(Action("but", ActionEnum.INPUT), State("s2"))
+        spec_root.children = [node1, node2]
+        node3 = Node(Action("liq", ActionEnum.OUTPUT), State("s3"))
+        node1.children = [node3]
+        node4 = Node(Action("but", ActionEnum.INPUT), State("s4"))
+        node2.children = [node4]
+        node5 = Node(Action("liq", ActionEnum.OUTPUT), State("s5"))
+        node4.children = [node5]
+        spec = LTSTree(spec_root)
+        spec.trace()
+
+        self.assertFalse(imp.io_conform2(spec), msg="Error, test result: io conform!")
+
+
+if __name__ == "__main__":
+    unittest.main()
